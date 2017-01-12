28240f2ca2c725e7b4cab907a22ec84bfa0dd8f3
Evan Anderson <epanderson92@yahoo.com>
2017-01-12 10:57:22 -0600

    serach,js

---
+++ app/Components/children/Search.js
@@ -0,0 +1,24 @@
+var React = require("react");
+
+// search history
+
+var History = React.createClass({
+
+  // Here we render the function
+  render: function() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title text-center">Search History</h3>
+        </div>
+        <div className="panel-body text-center">
+          <h1>Address:</h1>
+          <p>{this.props.address}</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+// Export the component back for use in other files
+module.exports = History;
